2b70037040f186a31ae0bb352ebecf8bee6847ea
59de44955ebd <59de44955ebd@gmail.com>
2024-07-04 10:05:11 +0200

    some cleanup

--- map/main.js
+++ map/main.js
@@ -3,10 +3,8 @@
 const BING_KEY = 'AqRX_rx3tLFIIRmRsW8ZY2iJfj84IxUPzesU36Fr-y8JROPionQJN82GwuP71SJ7';
 const HERE_KEY = 'ntjooxhxtB9vA0vznumOIytglteLGuFE91VaE56YOUk';
 const MAPBOX_KEY = 'pk.eyJ1IjoidnNsNDIiLCJhIjoiY2xha3o1ZmZ0MDA4ZDN2bXMzcnIweWhhcCJ9.IU5zt8kMIRsIhfKJWpgbgg';
-//const MAPQUEST_KEY = 'ixwZTkGHTFTvo1XYZfiyEYw9o3YVe7Jv';
 const MAPTILER_KEY = 'ppaPBnSs4o1IOGtXW8oI';
 const THUNDERFOREST_KEY = 'c6644d4c8d6f4bd18067c449f978a779';
-//const YANDEX_KEY = 'eb058d95-c62a-427a-b752-07cae5d9a735';
 
 const cachable_layers = {
 	'OpenTopoMap': 'otm',
@@ -14,7 +12,6 @@ const cachable_layers = {
 	'OpenCycleMap (Thunderforest)': 'opencyclemap',
 	'Outdoors (Thunderforest)': 'outdoors',
 	'Landscape (Thunderforest)': 'landscape',
-	//'openaerialmap' => 'https://api.mapbox.com/styles/v1/openaerialmap/ciyx28hy800362rto0u9x10fv/tiles/256', # max 18
 	'ÖPNVKarte': 'opnv',
 	'CycleOSM': 'cyclosm',
 	'Topographic Map France (IGN)': 'ign',
@@ -35,7 +32,6 @@ const base_maps = {
 
     'OSM': L.tileLayer(
         'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-        // 'https://map.dasdeck.com/tiles/osm/{z}/{x}/{y}.png',
         {
 	        subdomains:['a', 'b', 'c'],
 	        maxNativeZoom: 19,
@@ -61,7 +57,6 @@ const base_maps = {
 
     'OpenTopoMap': L.tileLayer(
         'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png',
-//        'https://map.dasdeck.com/tiles/otm/{z}/{x}/{y}.png',
         {
 	        subdomains:['a', 'b', 'c'],
 	        //maxZoom: 16,
@@ -73,7 +68,6 @@ const base_maps = {
 
 	'OpenCycleMap (Thunderforest)': L.tileLayer(
 	    'https://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png?apikey=' + THUNDERFOREST_KEY,
-	    //'https://map.dasdeck.com/tiles/opencyclemap/{z}/{x}/{y}.png',
 	    {
 		    subdomains:['a', 'b', 'c'],
 	        //maxZoom: 19, ???
@@ -85,7 +79,6 @@ const base_maps = {
 
     'Outdoors (Thunderforest)': L.tileLayer(
     	'https://{s}.tile.thunderforest.com/outdoors/{z}/{x}/{y}.png?apikey=' + THUNDERFOREST_KEY,
-    	//'https://map.dasdeck.com/tiles/outdoors/{z}/{x}/{y}.png',
     	{
 	        subdomains:['a', 'b', 'c'],
 	        //maxZoom: 19, ???
@@ -95,7 +88,6 @@ const base_maps = {
 	    }
     ),
 
-    //'https://map.dasdeck.com/tiles/landscape/{z}/{x}/{y}.png'
     'Landscape (Thunderforest)': L.tileLayer(
     	'https://{s}.tile.thunderforest.com/landscape/{z}/{x}/{y}.png?apikey=' + THUNDERFOREST_KEY, {
         subdomains:['a', 'b', 'c'],
@@ -107,7 +99,6 @@ const base_maps = {
 
     'ÖPNVKarte': L.tileLayer(
     	'https://tileserver.memomaps.de/tilegen/{z}/{x}/{y}.png',
-    	//'https://map.dasdeck.com/tiles/opnv/{z}/{x}/{y}.png',
     	{
 	        //maxZoom: 19,
 	        maxNativeZoom: 19,
@@ -119,7 +110,6 @@ const base_maps = {
 
     'CycleOSM': L.tileLayer(
     	'https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png',
-    	//'https://map.dasdeck.com/tiles/cyclosm/{z}/{x}/{y}.png',
     	{
 	        subdomains:['a', 'b', 'c'],
 	        //maxZoom: 19,
@@ -131,7 +121,6 @@ const base_maps = {
 
     'Topographic Map France (IGN)': L.tileLayer(
         'https://wxs.ign.fr/an7nvfzojv5wa96dsga5nk8w/geoportail/wmts?layer=GEOGRAPHICALGRIDSYSTEMS.MAPS.SCAN25TOUR.CV&style=normal&tilematrixset=PM&Service=WMTS&Request=GetTile&Version=1.0.0&Format=image%2Fjpeg&TileMatrix={z}&TileCol={x}&TileRow={y}',
-//        'https://map.dasdeck.com/tiles/ign/{z}/{x}/{y}.jpeg',
         {
 	        //maxZoom: 16,
 	        maxNativeZoom: 16,
@@ -288,18 +277,12 @@ const base_maps = {
 	        attribution: '© 2022 MapQuest | <a href="https://hello.mapquest.com/terms-of-use">Terms</a>',
 	    }
     ),
-
-    //'OpenAerialMap': openaerialmap,
-    //'IGN-Aerial (FR)': ign_aerial, // same as OpenAerialMap
-    //  'Apple': apple,
-    //  https://gitlab.com/IvanSanchez/Leaflet.MapkitMutant
 };
 
 const overlay_maps = {
 
     'Hiking (Waymarked Trails)': L.tileLayer(
         'https://tile.waymarkedtrails.org/hiking/{z}/{x}/{y}.png',
-//        'https://map.dasdeck.com/tiles/hiking/{z}/{x}/{y}.png',
         {
 	        maxNativeZoom: 16, // 18
 	        maxZoom: 20,
@@ -308,7 +291,6 @@ const overlay_maps = {
 
     'Cycling (Waymarked Trails)': L.tileLayer(
         'https://tile.waymarkedtrails.org/cycling/{z}/{x}/{y}.png',
-        //'https://map.dasdeck.com/tiles/cycling/{z}/{x}/{y}.png',
         {
 	        maxNativeZoom: 16,
 	        maxZoom: 20,
@@ -317,7 +299,6 @@ const overlay_maps = {
 
     'OpenSeaMap': L.tileLayer(
     	'https://t1.openseamap.org/seamark/{z}/{x}/{y}.png',
-    	//'https://map.dasdeck.com/tiles/openseamap/{z}/{x}/{y}.png',
     	{
 	        maxNativeZoom: 16,
 	        maxZoom: 20,
@@ -365,12 +346,11 @@ if (window.location.hash.startsWith('#map='))
 if (!lng)
 {
     // Berlin
-    zoom = 19;
-    lat = 52.541522823465684;
-    lng = 13.417739868164064;
+    zoom = 12;
+    lat = 52.51350476109457;
+    lng = 13.409671783447267;
 }
 
-//console.log(`${zoom}  ${lat}  ${lng}  ${base}`);
 window.location.hash = `map=${zoom}/${lat}/${lng}/${base}/${overlay}`;
 
 const map = L.map('map', {
@@ -384,16 +364,8 @@ const map = L.map('map', {
 L.control.layers(base_maps, overlay_maps, {position: 'topleft'}).addTo(map);
 
 L.control.scale().addTo(map);
-//map.addControl(new L.Control.Fullscreen({position: 'topright'}));
 
 L.control.measure({position: 'topleft'}).addTo(map);
-
-//var measureAction = new L.MeasureAction(map, {
-//    model: "distance", // 'area' or 'distance', default is 'distance'
-//});
-// measureAction.setModel('area');
-//measureAction.enable();
-
 
 map.addControl( new L.Control.Search({
 	url: 'https://nominatim.openstreetmap.org/search?format=json&accept-language=de-DE&q={s}',
@@ -405,15 +377,10 @@ map.addControl( new L.Control.Search({
 	autoCollapse: true,
 	minLength: 2,
 	zoom:10,
-//	text: 'Suchen...',
-//	textCancel: 'Abbrechen',
-//	textErr: 'Kein Ergebnis gefunden'
 }) );
 
 L.control.locate({
 	follow: true,
-	//title: 'Position bestimmen',
-	//popupText: ['Sie befinden sich innerhalb ', ' von diesem Punkt']
 }).addTo(map);
 
 if (overlay)
@@ -470,7 +437,6 @@ map.on('baselayerchange', function(evt) {
     window.location.hash = `map=${map.getZoom()}/${p.lat}/${p.lng}/${base}/${overlay}`;
 });
 
-
 map.on('overlayadd', function(evt) {
     overlay = evt.name;
     const p = map.getCenter()
@@ -495,8 +461,6 @@ function deg_to_rad(deg) {
 }
 
 document.body.addEventListener("keydown", (evt) => {
-//	console.log(evt);
-
 	if ((overlay || last_overlay) && evt.keyCode == 32)
 	{
 		space_toggled = !space_toggled;
@@ -514,81 +478,5 @@ document.body.addEventListener("keydown", (evt) => {
 	{
     	const p = map.getCenter()
     	window.open(`https://www.google.com/maps/@${p.lat},${p.lng},220m/data=!3m1!1e3`);
-	}
-
-	// ctrl
-	if (evt.keyCode == 17 && !evt.repeat)
-	{
-		if (!cachable_layers[base])
-				return;
-
-		if (rect)
-		{
-			rect.disableEdit();
-
-			const r = rect.getLatLngs()[0];
-
-//			console.log(r);
-//			return;
-
-			rect.remove();
-			shades.remove();
-			rect = null;
-
-			//const zoom = 15; //map.getZoom()
-			const zoom = parseInt(window.prompt("Zoom level?", "15"));
-
-			const n = Math.pow(2, zoom);
-
-			let xtile1 = Math.floor(n * ((r[2]['lng'] + 180) / 360));
-			let xtile2 = Math.ceil(n * ((r[0]['lng'] + 180) / 360));
-
-			let ytile1 = Math.floor(n * (1 - Math.asinh(Math.tan(deg_to_rad(r[0]['lat']))) / Math.PI) / 2);
-			let ytile2 = Math.ceil(n * (1 - Math.asinh(Math.tan(deg_to_rad(r[2]['lat']))) / Math.PI) / 2);
-
-			console.log('Number of tiles:', (xtile2 - xtile1 + 1) * (ytile2 - ytile1 + 1));
-
-			fetch('tiles/cache.php', {
-			    method: 'POST',
-			    headers: {
-//			      'Accept': 'application/json',
-			      'Content-Type': 'application/json'
-			    },
-			    body: JSON.stringify({
-			    	x1: xtile1,
-			    	x2: xtile2,
-
-			    	y1: ytile1,
-			    	y2: ytile2,
-
-			    	z: zoom,
-			    	layer: cachable_layers[base],
-			    	overlay: cachable_layers[overlay],
-			    }),
-			})
-	        .then(response => response.blob())
-	        .then(blob => {
-	            const url = window.URL.createObjectURL(blob);
-	            const a = document.createElement('a');
-	            a.href = url;
-	            a.download = "tiles.zip";
-	            document.body.appendChild(a);
-	            a.click();
-	            a.remove();
-	        });
-		}
-		else
-		{
-			shades.addTo(map);
-			rect = map.editTools.startRectangle();
-		}
-	}
-
-	// esc
-	if (evt.keyCode == 27 && rect)
-	{
-		rect.remove();
-		shades.remove();
-		rect = null;
 	}
 });
